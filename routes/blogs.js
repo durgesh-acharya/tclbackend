bd579aec2cfdc22fc1738b9bd8ac9c9ac88ae0c2
durgesh <durgesh.acharya2006@gmail.com>
2025-05-05 17:21:32 +0530

    rectified blogs routes

--- routes/blogs.js
+++ routes/blogs.js
@@ -8,45 +8,42 @@ const bodyParser = require('body-parser');
 router.use(bodyParser.json());
 
 // Create blog post with image
-router.post('/create', upload.single('image'), async (req, res) => {
-  try {
-    const { blogs_title, blogs_content } = req.body;
-    const blogs_imageurl = req.file ? `/assets/${req.file.filename}` : null;
+router.post('/create', upload.single('image'), (req, res) => {
 
-    const [result] = await db.execute(
-      'INSERT INTO blogs (blogs_title, blogs_content, blogs_imageurl, blogs_datetime) VALUES (?, ?, ?, NOW())',
-      [blogs_title, blogs_content, blogs_imageurl]
-    );
+  const { blogs_title, blogs_content } = req.body;
+  const blogs_imageurl = req.file ? `/assets/${req.file.filename}` : null;
 
-    res.status(201).json({
-      status: true,
-      message: 'Blog created',
-      data: { blog_id: result.insertId }
-    });
-  } catch (err) {
-    res.status(500).json({ status: false, message: err.message });
-  }
+  const query = `
+  INSERT INTO blogs (blogs_title, blogs_content, blogs_imageurl, blogs_datetime) VALUES (?, ?, ?, NOW())
+  `;
+
+  db.query(query, [blogs_title, blogs_content, blogs_imageurl], (err, result) => {
+    if (err) {
+      return res.status(500).json({ status: false, message: 'Error creating blogs', data: [] });
+    }
+
+    handleResponse(err, [{ blogs_id: result.insertId }], res);
+  });
 });
 
+
+
 // Fetch all blogs
-router.get('/', async (req, res) => {
-  try {
-    const [rows] = await db.execute('SELECT * FROM blogs ORDER BY blogs_datetime DESC');
-    handleResponse(null, rows, res);
-  } catch (err) {
-    handleResponse(err, [], res);
-  }
+router.get('/',(req, res) => {
+
+  db.query('SELECT * FROM blogs ORDER BY blogs_datetime DESC', (err, results) => {
+    handleResponse(err, results, res);
+  });
 });
 
 // Fetch blog by ID
-router.get('/:id', async (req, res) => {
-  try {
-    const [rows] = await db.execute('SELECT * FROM blogs WHERE blogs_id = ?', [req.params.id]);
-    handleResponse(null, rows, res);
-  } catch (err) {
-    handleResponse(err, [], res);
-  }
+router.get('/:id',(req, res) => {
+  const blogid = req.params.id;
+  db.query('SELECT * FROM blogs WHERE blogs_id = ?', [blogid ], (err, results) => {
+    handleResponse(err, results, res);
+  });
 });
+
 
 // Delete blog by ID
 router.delete('/:id', async (req, res) => {
